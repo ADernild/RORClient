0a9a189596c1f57b817744830e4aaded216aa0b0
ADernild <alex@dernild.dk>
2025-02-18 00:23:13 +0100

    data models to hold and validate response data

---
+++ rorclient/models/__init__.py
@@ -0,0 +1,32 @@
+"""
+Copyright (c) 2025 ADernild
+
+Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+Author: ADernild
+Email: alex@dernild.dk
+Project: RORClient
+Description: Initialization file for the RORClient models package.
+Date: 2025-02-17
+"""
+
+from .admin import Admin, AdminCreated, AdminLastModified
+from .external_id import ExternalId
+from .institution import Institution
+from .link import Link
+from .location import GeonamesDetails, Location
+from .name import Name
+from .relationship import Relationship
+
+__all__ = [
+    "Admin",
+    "AdminCreated",
+    "AdminLastModified",
+    "ExternalId",
+    "Link",
+    "Location",
+    "GeonamesDetails",
+    "Name",
+    "Relationship",
+    "Institution",
+]
---
+++ rorclient/models/link.py
@@ -0,0 +1,26 @@
+"""
+Copyright (c) 2025 ADernild
+
+Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+Author: ADernild
+Email: alex@dernild.dk
+Project: RORClient
+Description: Pydantic models for links in the ROR API.
+Date: 2025-02-17
+"""
+
+from pydantic import BaseModel, HttpUrl
+
+
+class Link(BaseModel):
+    """
+    The organization's website and Wikipedia page.
+
+    Attributes:
+        type (str): The type of the link (e.g., website, wikipedia).
+        value (HttpUrl): The URL of the link.
+    """
+
+    type: str
+    value: HttpUrl
