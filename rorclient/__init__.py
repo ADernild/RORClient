9e17e15469660f70496c41d8b6edf39894fdad01
ADernild <alex@dernild.dk>
2025-02-18 00:22:09 +0100

    start of repo

---
+++ rorclient/__init__.py
@@ -0,0 +1,11 @@
+"""
+Copyright (c) 2025 ADernild
+
+Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+Author: ADernild
+Email: alex@dernild.dk
+Project: RORClient
+Description: Initialization file for the RORClient package.
+Date: 2025-02-17
+"""
